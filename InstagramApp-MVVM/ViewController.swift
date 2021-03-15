b579f2a842996ebe140af3cf126acea2559ba81c
Justyna <kowalkowska.just@gmail.com>
2021-03-15 21:00:30 +0100

    Initial commit.

---
+++ InstagramApp-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramApp-MVVM
+//
+//  Created by Justyna Kowalkowska on 15/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
