f26578fb7e17df0df9e925ff4dfafe048c926fe9
Justyna <kowalkowska.just@gmail.com>
2021-03-16 02:10:57 +0100

    Creating an UIView Extension with functions for add constraints.

---
+++ InstagramApp-MVVM/Utils/Extension - UIView - Constraints.swift
@@ -0,0 +1,8 @@
+//
+//  Extension - UIView - Constraints.swift
+//  InstagramApp-MVVM
+//
+//  Created by Justyna Kowalkowska on 16/03/2021.
+//
+
+import Foundation
---
+++ InstagramApp-MVVM/View/FeedCell.swift
@@ -0,0 +1,12 @@
+//
+//  FeedCell.swift
+//  InstagramApp-MVVM
+//
+//  Created by Justyna Kowalkowska on 16/03/2021.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+}
