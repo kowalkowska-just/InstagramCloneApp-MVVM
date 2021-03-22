18a3a58bc7c607a8fbbdd69f8f0b2ec41189c60c
Justyna <kowalkowska.just@gmail.com>
2021-03-22 18:01:06 +0100

    Creating a ProfileCell view.

--- InstagramApp-MVVM/View/Profile/ProfileCell.swift
+++ InstagramApp-MVVM/View/Profile/ProfileCell.swift
@@ -11,13 +11,20 @@ class ProfileCell: UICollectionViewCell {
     
     //MARK: - Properties
     
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
     
     //MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .lightGray
+        configureView()
     }
     
     required init?(coder: NSCoder) {
@@ -26,6 +33,12 @@ class ProfileCell: UICollectionViewCell {
     
     //MARK: - Helper Functions
     
-    
+    private func configureView() {
+        
+        backgroundColor = .lightGray
+
+        addSubview(postImageView)
+        postImageView.fillSuperview()
+    }
 }
 
