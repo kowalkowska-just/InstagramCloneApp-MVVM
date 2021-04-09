8df63d61961d3d2bcbe1b8dd73e4250a284e9a68
Justyna <kowalkowska.just@gmail.com>
2021-04-09 22:23:42 +0200

    Creating a UserCell for SearchController.

--- InstagramApp-MVVM/Controller/SearchController.swift
+++ InstagramApp-MVVM/Controller/SearchController.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
-class SearchController: UIViewController {
+private let reuseIdentifier = "UserCell"
+
+class SearchController: UITableViewController {
     
     //MARK: - Properties
     
@@ -15,9 +17,30 @@ class SearchController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
 
+        configureTableView()
     }
     
     //MARK: - Helper Functions
+    
+    private func configureTableView() {
+        view.backgroundColor = .white
+        
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 64
+    }
+}
+
+//MARK: - UITableViewDataSource and UITableViewDelegate
+
+extension SearchController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        return cell
+    }
 }
---
+++ InstagramApp-MVVM/View/UserCell.swift
@@ -0,0 +1,67 @@
+//
+//  UserCell.swift
+//  InstagramApp-MVVM
+//
+//  Created by Justyna Kowalkowska on 09/04/2021.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+    
+    //MARK: - Properties
+    
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        return iv
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "venom"
+        return label
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "Eddie Brock"
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    //MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Helper Functions
+    
+    private func setupView() {
+        
+        addSubview(profileImageView)
+        profileImageView.setDimensions(height: 48, width: 48)
+        profileImageView.layer.cornerRadius = 48 / 2
+        profileImageView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        
+        let stack = UIStackView(arrangedSubviews: [usernameLabel, fullnameLabel])
+        stack.axis = .vertical
+        stack.spacing = 4
+        stack.alignment = .leading
+        
+        addSubview(stack)
+        stack.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 8)
+    }
+}
