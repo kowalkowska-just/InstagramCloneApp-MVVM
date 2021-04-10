60c454ae326e70d9e15045d2925a28f9d3076935
Justyna <kowalkowska.just@gmail.com>
2021-04-10 21:16:38 +0200

    Adding a UISearchController in SearchController and searching for users.

--- InstagramApp-MVVM/Controller/SearchController.swift
+++ InstagramApp-MVVM/Controller/SearchController.swift
@@ -14,6 +14,12 @@ class SearchController: UITableViewController {
     //MARK: - Properties
     
     private var users = [User]()
+    private var filteredUsers = [User]()
+    private let searchController = UISearchController(searchResultsController: nil)
+    
+    private var inSearchMode: Bool {
+        return searchController.isActive && !searchController.searchBar.text!.isEmpty
+    }
     
     //MARK: - Lifecycle
     
@@ -21,6 +27,7 @@ class SearchController: UITableViewController {
         super.viewDidLoad()
 
         configureTableView()
+        configureSearchController()
         fetchUsers()
     }
     
@@ -41,6 +48,15 @@ class SearchController: UITableViewController {
         tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.rowHeight = 64
     }
+    
+    private func configureSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        definesPresentationContext = false
+    }
 }
 
 //MARK: - UITableViewDataSource
@@ -48,12 +64,13 @@ class SearchController: UITableViewController {
 extension SearchController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        return inSearchMode ? filteredUsers.count : users.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
-        cell.viewModel = UserCellViewModel(user: users[indexPath.row])
+        let user = inSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
+        cell.viewModel = UserCellViewModel(user: user)
         return cell
     }
 }
@@ -66,4 +83,20 @@ extension SearchController {
         let controller = ProfileController(user: users[indexPath.row])
         navigationController?.pushViewController(controller, animated: true)
     }
+}
+
+//MARK: - UISearchResultsUpdating
+
+extension SearchController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        guard let searchText = searchController.searchBar.text?.lowercased() else { return }
+        print("DEBUG: Search text \(searchText)")
+        
+        filteredUsers = users.filter({ $0.username.contains(searchText) || $0.fullname.lowercased().contains(searchText) })
+        
+        print("Debug: Filtered users \(filteredUsers)")
+        self.tableView.reloadData()
+    }
+
 }
