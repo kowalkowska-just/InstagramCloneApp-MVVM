683b45461517571296f55b78f7687b1551ee9208
Justyna <kowalkowska.just@gmail.com>
2021-04-29 23:46:43 +0200

    Fixing bug with checking if user liked posts.

--- InstagramApp-MVVM/API/PostService.swift
+++ InstagramApp-MVVM/API/PostService.swift
@@ -68,15 +68,16 @@ struct PostService {
         COLLECTION_POSTS.document(post.postId).updateData(["likes": post.likes - 1])
         
         COLLECTION_POSTS.document(post.postId).collection("post-like").document(uid).delete { _ in
-            COLLECTION_POSTS.document(uid).collection("user-likes").document(post.postId).delete(completion: completion)
+            COLLECTION_USERS.document(uid).collection("user-likes").document(post.postId).delete(completion: completion)
         }
     }
     
     static func checkIfUserLikedPost(post: Post, completion: @escaping(Bool) -> Void) {
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
-        COLLECTION_POSTS.document(uid).collection("user-likes").document(post.postId).getDocument { (snapshot, _) in
-            guard let didLike = snapshot?.exists else { return }
+        COLLECTION_USERS.document(uid).collection("user-likes").document(post.postId).getDocument { (snapshot, _) in
+            guard let snapshot = snapshot else { return }
+            let didLike = snapshot.exists
             completion(didLike)
         }
     }
