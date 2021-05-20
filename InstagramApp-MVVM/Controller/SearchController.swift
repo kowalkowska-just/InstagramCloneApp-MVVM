bfcb24043873b2b7f647106a27919abeb51fc409
Justyna <kowalkowska.just@gmail.com>
2021-05-20 14:09:51 +0200

    Changing a UITableViewController to UIViewController in SearchController and setting up tableView.

--- InstagramApp-MVVM/Controller/SearchController.swift
+++ InstagramApp-MVVM/Controller/SearchController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 private let reuseIdentifier = "UserCell"
 
-class SearchController: UITableViewController {
+class SearchController: UIViewController {
     
     //MARK: - Properties
+    
+    private let tableView = UITableView()
     
     private var users = [User]()
     private var filteredUsers = [User]()
@@ -43,10 +45,17 @@ class SearchController: UITableViewController {
     //MARK: - Helper Functions
     
     private func configureTableView() {
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         view.backgroundColor = .white
         
         tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.rowHeight = 64
+        
+        view.addSubview(tableView)
+        tableView.fillSuperview()
     }
     
     private func configureSearchController() {
@@ -61,13 +70,13 @@ class SearchController: UITableViewController {
 
 //MARK: - UITableViewDataSource
 
-extension SearchController {
+extension SearchController: UITableViewDataSource {
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return inSearchMode ? filteredUsers.count : users.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
         let user = inSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
         cell.viewModel = UserCellViewModel(user: user)
@@ -77,8 +86,8 @@ extension SearchController {
 
 //MARK: - UITableViewDelegate
 
-extension SearchController {
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+extension SearchController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("DEBUG: User is \(users[indexPath.row].username)")
         let controller = ProfileController(user: users[indexPath.row])
         navigationController?.pushViewController(controller, animated: true)
